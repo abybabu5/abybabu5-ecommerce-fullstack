fe4ab433f18d34cbd80261980c7bd323790bebe1
Aby Babu <abyStriveschool@gmail.com>
2021-01-25 13:35:15 +0100

    footer added

--- src/Redux/StateProvider.js
+++ src/Redux/StateProvider.js
@@ -1,5 +1,5 @@
 import React, {createContext, useContext, useReducer} from "react";
-//prepares the data layout
+//prepares the data layer
 export  const StateContext = createContext();
 //Wrap our app and provide the data layer to the components
 export const StateProvider = ({reducer, initialState, children}) => (
